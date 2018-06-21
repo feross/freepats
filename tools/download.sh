6a14600b06820b63f62f79846fb02ad12434ba7f
Feross Aboukhadijeh <feross@feross.org>
2018-06-20 17:17:23 -0700

    improve download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -2,18 +2,23 @@
 # Download and extract instrument patch files
 set -e
 
+rm -rf freepats Drum_000 Tone_000 freepats.cfg LICENSE README_FREEPATS.txt
+
 curl 'http://freepats.zenvoid.org/freepats-20060219.tar.xz' -o /tmp/freepats-20060219.tar.xz
 tar xf /tmp/freepats-20060219.tar.xz
 
 mv freepats/Drum_000 .
 mv freepats/Tone_000 .
 mv freepats/freepats.cfg .
-mv freepats/COPYING ./LICENSE
-mv freepats/README ./README_FREEPATS.txt
+mv freepats/COPYING LICENSE
+mv freepats/README README_FREEPATS.txt
 
-# Remove extra information about each sound. Important config info for each
+# Remove txt files with info about each sound. Important config info for each
 # instrument is already present in freepats.cfg, so txt files are not needed.
 rm Drum_000/*.txt
 rm Tone_000/*.txt
+
+# Remove comments from the timidity config file to save bytes
+sed -i '/^#/d' freepats.cfg
 
 rm -rf freepats
